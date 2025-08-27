eeb02aef633d0624573eaa7423f6abcb0d5d45d4
lek <abungan082198@gmail.com>
2025-08-28 00:34:21 +0800

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter, Routes, Route } from "react-router-dom";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import "./app.css";
 import App from "./App.jsx";
-
 import EventForm from "./components/EventForm.jsx";
 import Events from "./pages/Events.jsx";
 import NotFoundPage from "./pages/NotFoundPage.jsx";
+
+import "./app.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
